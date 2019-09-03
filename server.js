707eeb0845f32e499688481076290e491050006a
Niclas Olsson <nicolss@kth.se>
2019-09-03 12:33:34 +0200

    gogo!

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/angular-performance-test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(console.log(process.env.PORT || 8080));
+app.listen(process.env.PORT || 8080);
